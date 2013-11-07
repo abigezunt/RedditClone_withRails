00bb6c82280b334a7b62cae3d33097ab90a8c1e2
Abby Howell <abby.g.howell@gmail.com>
2013-11-07 18:21:36 -0500

    copying over and modifying views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :category
+	delegate :name, :to => :category, :prefix => true
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true
 end
