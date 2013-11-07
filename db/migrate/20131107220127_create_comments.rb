6a28c736b31ac48f3fb8c89277d152fec829c9d2
Abby Howell <abby.g.howell@gmail.com>
2013-11-07 17:13:03 -0500

    working on copying over stylesheets and views from old Reddit project

--- db/migrate/20131107220127_create_comments.rb
+++ db/migrate/20131107220127_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.string :author
+      t.string :author, default: 'anonymous'
       t.text :body
       t.references :post, index: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107214751) do
+ActiveRecord::Schema.define(version: 20131107220127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20131107214751) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author",     default: "anonymous"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: true do |t|
     t.string   "title"
